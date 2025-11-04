f80d543525e80be2f6c36708d003551bac5b01f7
Inajara <inajaralemos1@gmail.com>
2025-11-03 22:08:57 -0300

    .

--- Samples/ProjetoFinal/DTO/ClasseDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjetoFinal.DTO;
-
-public class ClasseDTO
-{
-    public Guid Id { get; set; }
-    public string NomeClasse { get; set; }
-    public AtributosDTO Atributo { get; set; }
-    public List<LinkDTO> Links { get; set; } = new();
-}
--- Samples/ProjetoFinal/Models/Login.cs
+++
@@ -1,7 +0,0 @@
-namespace ProjetoFinal.Models;
-
-public class Login
-{
-    public string Usuario { get; set; }
-    public string Senha { get; set; }
-}
